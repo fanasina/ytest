8737589be47b3175b37822311f2b052a4f9b25aa
fanasina <rafanasin@gmail.com>
2023-08-26 22:24:41 +0200

    modify compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc src/test_t/is_good.c src/test_t/test_t.c src/tools_t/tools_t.c \
+gcc is_good.c src/test_t/test_t.c src/tools_t/tools_t.c \
   src/permutation_t/permutation_t.c src/set_theoric_t/set_theoric_t.c \
-  -I./src -o isgood && ./isgood -h -p
+  -I./src -o lunch_is_good_c && ./lunch_is_good_c -h -p
