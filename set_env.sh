537d34bca0038748dcb41325a8a06bef789e8519
fanasina <rafanasin@gmail.com>
2023-10-23 20:36:05 +0200

    update set and unset env

--- set_env.sh
+++ set_env.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+export CPATH=$PWD/include_ytest/include:$CPATH
+export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
+
 if [ "$#" -le 0 ] ; then
   echo "if need permanent config"
   echo "Usage: $0 file_name" >&2
   echo "for example: $0 ~/.bashrc" >&2
   echo "or an other bash profile, here it is \"~/.bashrc\" for example"
-  export CPATH=$PWD/include_ytest/include:$CPATH
-  export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
 else
   echo "export CPATH=$PWD/include_ytest/include:\$CPATH" >> $1
   echo "export LD_LIBRARY_PATH=$PWD:\$LD_LIBRARY_PATH" >> $1
--- unset_env.sh
+++ unset_env.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+
+LD_LIBRARY_PATH=$(echo $LD_LIBRARY_PATH | sed "s@$PWD:@@g")
+CPATH=$(echo $CPATH | sed "s@$PWD/include_ytest/include:@@g")
+
 if [ "$#" -le 0 ] ; then
-  LD_LIBRARY_PATH=$(echo $LD_LIBRARY_PATH | sed "s@$PWD:@@g")
-  CPATH=$(echo $CPATH | sed "s@$PWD/include_ytest/include:@@g")
   echo "If need permanent unset env "
   echo "Usage: $0 file_name"
   echo "for example: $0 ~/.bashrc" >&2
-  echo "or other bash profile ( here : \"~/.bashrc\" for example), the same file you have set whith set_env.sh"
+  echo "or other bash profile ( here : \"~/.bashrc\" for example), the same file you have set with set_env.sh"
 fi
 
 
