3430407a93f1f7a2ef52ea54be8486991e4e1ccc
fanasina <rafanasin@gmail.com>
2023-10-26 09:45:22 +0200

    create dimension_t

---
+++ permutation_test/src/dimension_t/dimension_t.h
@@ -0,0 +1,8 @@
+#ifndef __DIMENSION_T__H__
+#define __DIMENSION_T__H__
+
+#include "permutation_t/permutation_t.h"
+
+typedef dimension struct PERMUTATION_TYPE_SIZE_T;
+
+//int compare_dimension(dimension *d1, dimension *d2);
